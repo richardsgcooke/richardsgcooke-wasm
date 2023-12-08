8110f4bad08ab3d3bafffb7e4eec027f1d9a34cc
LinearDynamics <32169863+LinearDynamics@users.noreply.github.com>
2023-12-08 08:50:52 -0500

    Add project files.

---
+++ richardsgcooke-wasm/Models/ProjectItem.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+public class ProjectItem
+{
+    public string Title { get; set; }
+    public MarkupString Description { get; set; }
+    public List<string>? ListThings { get; set; }
+}
